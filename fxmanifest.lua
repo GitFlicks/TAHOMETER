80a8c11cde9f2fa7dec54d5c5cd613a1d967f274
Flicks <kroxeen@gmail.com>
2023-05-13 17:46:39 +0200

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version('cerulean')
+author {'Flicks'}
+games({ 'gta5' })
+
+
+ui_page{
+    'html/index.html'
+}
+
+files{
+    'html/index.html',
+    'html/style.css',
+    'html/index.js'
+}
+
+client_scripts({
+    'client.lua'
+});
